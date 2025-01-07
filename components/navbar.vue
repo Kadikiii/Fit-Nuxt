8704673cd3f7933878631512e28477d012e7262a
Mustafa Kadiki <149871350+Kadikiii@users.noreply.github.com>
2025-01-07 21:22:25 +0200

    added pages

---
+++ components/navbar.vue
@@ -0,0 +1,47 @@
+
+<template>
+    <nav class="bg-white dark:bg-gray-800 antialiased">
+        <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0 py-4">
+          <div class="flex items-center justify-between">
+      
+            <div class="flex items-center space-x-8">
+              <div class="shrink-0">
+                <a href="/" title="" class="">
+                  <img class="block w-auto h-8 dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full.svg" alt="">
+                  <img class="hidden w-auto h-8 dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full-dark.svg" alt="">
+                </a>
+              </div>
+      
+              <ul class=" lg:flex items-center justify-start gap-6 md:gap-8 py-3 sm:justify-center">
+                <li>
+                  <a href="#" title="" class="flex text-sm font-medium text-gray-900 hover:text-primary-700 dark:text-white dark:hover:text-primary-500">
+                    Home
+                  </a>
+                </li>
+                <li class="shrink-0">
+                  <a href="#" title="" class="flex text-sm font-medium text-gray-900 hover:text-primary-700 dark:text-white dark:hover:text-primary-500">
+                    Dashboard
+                  </a>
+                </li>
+              </ul>
+            </div>
+      
+
+          </div>
+      
+          <div id="ecommerce-navbar-menu-1" class="bg-gray-50 dark:bg-gray-700 dark:border-gray-600 border border-gray-200 rounded-lg py-3 hidden px-4 mt-4">
+            <ul class="text-gray-900  text-sm font-medium dark:text-white space-y-3">
+              <li>
+                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Home</a>
+              </li>
+              <li>
+                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Best Sellers</a>
+              </li>
+              <li>
+                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Gift Ideas</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  css: [
+    '@/assets/first.css', // Ensure this path is correct
+  ],
 }
-
