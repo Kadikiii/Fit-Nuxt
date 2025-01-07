879462495cfc83a294b19bc6b07a468f71e57ec8
Mustafa Kadiki <149871350+Kadikiii@users.noreply.github.com>
2025-01-07 20:52:32 +0200

    added tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-      <NuxtRouteAnnouncer />
-      <NuxtWelcome />
+      <h1>Home</h1>
+      <p>This is the home page.</p>
+      <li><NuxtLink to="/login">About</NuxtLink></li>
+      <li><NuxtLink to="/signup">About</NuxtLink></li>
     </div>
   </template>
