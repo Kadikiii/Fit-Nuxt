fb8a12031a9a1bf814fa45da1b025bdc5c776362
Mustafa Kadiki <149871350+Kadikiii@users.noreply.github.com>
2025-01-07 21:27:02 +0200

    add

--- components/navbar.vue
+++ components/navbar.vue
@@ -25,22 +25,6 @@
                 </li>
               </ul>
             </div>
-      
-
-          </div>
-      
-          <div id="ecommerce-navbar-menu-1" class="bg-gray-50 dark:bg-gray-700 dark:border-gray-600 border border-gray-200 rounded-lg py-3 hidden px-4 mt-4">
-            <ul class="text-gray-900  text-sm font-medium dark:text-white space-y-3">
-              <li>
-                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Home</a>
-              </li>
-              <li>
-                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Best Sellers</a>
-              </li>
-              <li>
-                <a href="#" class="hover:text-primary-700 dark:hover:text-primary-500">Gift Ideas</a>
-              </li>
-            </ul>
           </div>
         </div>
       </nav>
